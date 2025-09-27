e48a28d90f05e307d12205f352378807633140cf
NoackNa <noacknad@web.de>
2025-09-27 15:16:40 +0200

    feat: add dummy asset, create default template and button, adjust global styles

---
+++ src/components/atoms/button/index.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export type ButtonProps = {
+  text: string
+  href: string
+}
+
+export const Button = ({ text, href }: ButtonProps) => {
+  return (
+    <Link
+      className="focus:outline-none text-white bg-green-700 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      href={href}
+    >
+      {text}
+    </Link>
+  )
+}
