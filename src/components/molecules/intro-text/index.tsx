fca9853416cda5d1357c481f9e3594fa7d96f6e0
NoackNa <noacknad@web.de>
2025-09-27 17:37:26 +0200

    refactor: extract IntroText

---
+++ src/components/molecules/intro-text/index.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/atoms/button'
+import { SectionTitle } from '@/components/atoms/section-title'
+
+export type IntroTextProps = {
+  title: string
+  description: string
+  buttonText?: string
+}
+
+const IntroText = ({ title, description, buttonText }: IntroTextProps) => {
+  return (
+    <article>
+      <SectionTitle text={title} />
+      <p>{description}</p>
+      {buttonText && <Button text={buttonText} href={'/'} />}
+    </article>
+  )
+}
+
+export default IntroText
