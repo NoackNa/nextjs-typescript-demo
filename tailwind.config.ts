49b02074a9d4498898f89f20202a7fd41566ecc0
NoackNa <noacknad@web.de>
2025-09-28 19:10:34 +0200

    refactor: improve tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,7 @@
 import { type Config } from 'tailwindcss'
 
 export default {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,scss}',
-  ],
+  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx},scss', './src/app/**/*.{js,ts,jsx,tsx,mdx,scss}'],
   theme: {
     screens: {
       sm: '376px',
