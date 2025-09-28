ff7fac61f51b22c9d34801bcac7150666ce11215
NoackNa <noacknad@web.de>
2025-09-28 17:17:58 +0200

    style: extract primary--button styles

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import './styles.scss'
 
 export type ButtonProps = {
   text: string
@@ -7,10 +8,7 @@ export type ButtonProps = {
 
 export const Button = ({ text, href }: ButtonProps) => {
   return (
-    <Link
-      className="w-max focus:outline-none text-white bg-green-700 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-      href={href}
-    >
+    <Link className="button--primary" href={href}>
       {text}
     </Link>
   )
