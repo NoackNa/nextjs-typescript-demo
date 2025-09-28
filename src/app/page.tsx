d023429305c57e7fa3242c1b889fb165214ddd31
NoackNa <noacknad@web.de>
2025-09-28 16:44:11 +0200

    feat: restructure start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,37 +8,14 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <DefaultTemplate>
         <Headline alignment="center" text="Demo Page" />
-        <Button text="ImageText Module" href="/demo/image-text" />
-        <Button text="StarRating" href="/demo/star-rating" />
+        <section className="flex flex-col items-center gap-4">
+          <Button text="ImageText Module" href="/demo/image-text" />
+          <Button text="StarRating" href="/demo/star-rating" />
+        </section>
       </DefaultTemplate>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
+        <Image src="/window.svg" alt="Window icon" width={16} height={16} />
+        <p>Welcome to my Next.js / TypeScript Basic Demo App</p>
       </footer>
     </div>
   )
